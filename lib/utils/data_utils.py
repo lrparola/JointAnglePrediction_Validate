28c0799ec07aa406881990efa09bfb82873540e2
Lauren Parola <88797808+lrparola@users.noreply.github.com>
2022-11-22 09:09:19 -0500

    Update data_utils.py

--- lib/utils/data_utils.py
+++ lib/utils/data_utils.py
@@ -5,6 +5,7 @@ from __future__ import division
 from configs import constants as _C
 
 import torch
+from torch.nn.functional import normalize
 
 from pdb import set_trace as st
 
@@ -15,19 +16,19 @@ def normalize_angle(angle, norm_dict):
     return angle
     
 def normalize_new_data(imu_data1, imu_data2):
-    acc1_mean = imu_data1[:, :3].mean(axis=0).append(np.mean(np.linalg.norm(imu_data1[:, :3])))
-    gyr1_mean = imu_data1[:, 3:].mean(axis=0).append(np.mean(np.linalg.norm(imu_data1[:, 3:])))
-    acc2_mean = imu_data2[:, :3].mean(axis=0).append(np.mean(np.linalg.norm(imu_data2[:, :3])))
-    gyr2_mean = imu_data2[:, 3:].mean(axis=0).append(np.mean(np.linalg.norm(imu_data2[:, 3:])))
+    acc1_mean = torch.mean(imu_data1[:, :3]).append(torch.mean(normalize(imu_data1[:, :3])))
+    gyr1_mean = torch.mean(imu_data1[:, 3:]).append(torch.mean(normalize(imu_data1[:, 3:])))
+    acc2_mean = torch.mean(imu_data2[:, :3]).append(torch.mean(normalize(imu_data2[:, :3])))
+    gyr2_mean = torch.mean(imu_data2[:, 3:]).append(torch.mean(normalize(imu_data2[:, 3:])))
     
     norm_dict = {}
     norm_dict['params'] = {}
     norm_dict['params']['x_mean'] = [acc1_mean, gyr1_mean, acc2_mean, gyr2_mean]
     
-    acc1_std = imu_data1[:, :3].std(axis=0).append(np.std(np.linalg.norm(imu_data1[:, :3])))
-    gyr1_std = imu_data1[:, 3:].std(axis=0).append(np.std(np.linalg.norm(imu_data1[:, 3:])))
-    acc2_std = imu_data2[:, :3].std(axis=0).append(np.std(np.linalg.norm(imu_data2[:, :3])))
-    gyr2_std = imu_data2[:, 3:].std(axis=0).append(np.std(np.linalg.norm(imu_data2[:, 3:])))
+    acc1_std = torch.std(imu_data1[:, :3]).append(torch.std(normalize(imu_data1[:, :3])))
+    gyr1_std = torch.std(imu_data1[:, 3:]).append(torch.std(normalize(imu_data1[:, 3:])))
+    acc2_std = torch.std(imu_data2[:, :3]).append(torch.std(normalize(imu_data2[:, :3])))
+    gyr2_std =torch.std(imu_data2[:, 3:]).append(torch.std(normalize(imu_data2[:, 3:])))
     norm_dict['params']['x_std'] = [acc1_std, gyr1_std, acc2_std, gyr2_std]
     
     return norm_dict
