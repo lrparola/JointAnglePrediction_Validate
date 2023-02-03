4ca3dc2aa1fa679be659154592b9050ebebfcef7
Lauren Parola <88797808+lrparola@users.noreply.github.com>
2023-02-03 10:12:40 -0500

    Add files via upload

--- lib/data/dloader.py
+++ lib/data/dloader.py
@@ -11,14 +11,35 @@ from skimage.util.shape import view_as_windows
 
 from pdb import set_trace as st
 
+class Normalizer:
+    def __init__(self, norm_dict):
+        self.x_mean = norm_dict['params']['x_mean'].detach().cpu()
+        self.x_std = norm_dict['params']['x_std'].detach().cpu()
+        self.y_mean = norm_dict['params']['y_mean'].detach().cpu()
+        self.y_std = norm_dict['params']['y_std'].detach().cpu()
 
+    def normalize_input(self, x):
+        return (x - self.x_mean) / self.x_std
+
+    def normalize_output(self, y):
+        return (y - self.y_mean) / self.y_std
+
+    def unnormalize_input(self, x):
+        return x * self.x_std + self.x_mean
+    
+    def unnormalize_output(self, y):
+        return y * self.y_std + self.y_mean
+        
 class Dataset(torch.utils.data.Dataset):
-    def __init__(self, label_pth, joint, norm_dict, **kwargs):
+    def __init__(self, label_pth, joint, norm_dict, is_train=True,is_remote=False, **kwargs):
         super(Dataset, self).__init__()
 
         self.labels = torch.load(label_pth)
         self.joint = joint
-        self.norm_dict = norm_dict
+        self.is_train = is_train
+        self.is_remote = is_remote
+
+        self.normalizer = Normalizer(norm_dict)
         self.prepare_sequence_batch(kwargs.get('input_length', 400))
         self.kwargs =kwargs
 
@@ -34,29 +55,40 @@ class Dataset(torch.utils.data.Dataset):
         indices = np.split(
             np.arange(0, seqs.shape[0]), group_perm[1:]
         )
+        
         for idx in range(len(seqs_unique)):
             indexes = indices[idx]
-            if indexes.shape[0] < seq_length: continue
-            chunks = view_as_windows(
-                indexes, (seq_length), step=seq_length // 4
-            )
-            start_finish = chunks[:, (0, -1)].tolist()
-            self.seq_indices += start_finish
+            if self.is_train:
+                if indexes.shape[0] < seq_length: continue
+                chunks = view_as_windows(
+                    indexes, (seq_length), step=seq_length // 4
+                )
+                start_finish = chunks[:, (0, -1)].tolist()
+                self.seq_indices += start_finish
+            else:
+                self.seq_indices += indexes[None, (0, -1)].tolist()
 
 
     def __len__(self):
         return len(self.seq_indices)
 
     def __getitem__(self, index):
-        return self.get_single_sequence(index,self.kwargs)
-
-
-    def get_single_sequence(self, index,kwargs):
+        if self.is_remote == True:
+            x = self.get_single_imu_sequence(index,self.kwargs)
+        else:
+            x = self.get_single_sequence(index,self.kwargs)
+        return x
+    def get_single_imu_sequence(self, index,kwargs):
         start_index, end_index = self.seq_indices[index]
         all_imu = self.labels['imu'][start_index:end_index+1]
-        all_angle = self.labels['angle'][start_index:end_index+1]
+        
 
+        if self.is_train == False:
+            activity_label = self.labels['label'][index]
+        else:
+            activity_label = 0
         imus, angles = [], []
+        
         for side in ['left', 'right']:
             trg_joint = side[0] + self.joint.lower()
             temp_imu = []
@@ -66,17 +98,49 @@ class Dataset(torch.utils.data.Dataset):
             if kwargs['validation_norm_dict'] == True:
                 self.norm_dict = normalize_new_data(*temp_imu)
                 
-            imu = process_imu_data(*temp_imu, self.norm_dict)
+            imu = process_imu_data(*temp_imu) #, self.norm_dict)
+            imu = self.normalizer.normalize_input(imu)
+            imus.append(imu)
+
+
+        imus = torch.stack(imus)
+
+
+        return imus,activity_label
+        
+    def get_single_sequence(self, index,kwargs):
+        start_index, end_index = self.seq_indices[index]
+        all_imu = self.labels['imu'][start_index:end_index+1]
+        all_angle = self.labels['angle'][start_index:end_index+1]
+
+        if self.is_train == False:
+            activity_label = self.labels['label'][index]
+        else:
+            activity_label = 0
+        imus, angles = [], []
+        
+        for side in ['left', 'right']:
+            trg_joint = side[0] + self.joint.lower()
+            temp_imu = []
+            for sensor in _C.DATA.JOINT_IMU_MAPPER[trg_joint]:
+                temp_imu.append(all_imu[:, _C.DATA.IMU_LIST.index(sensor)])    
+
+            if kwargs['validation_norm_dict'] == True:
+                self.norm_dict = normalize_new_data(*temp_imu)
+                
+            imu = process_imu_data(*temp_imu) #, self.norm_dict)
+            imu = self.normalizer.normalize_input(imu)
             imus.append(imu)
 
             angle = all_angle[:, _C.DATA.JOINT_LIST.index(trg_joint)]
-            angle = normalize_angle(angle, self.norm_dict)
+            angle = self.normalizer.normalize_output(angle)
+            # angle = normalize_angle(angle, self.norm_dict)
             angles.append(angle)
 
         imus = torch.stack(imus)
         angles = torch.stack(angles)
 
-        return imus, angles
+        return imus, angles,activity_label
 
 
 def setup_validation_data(norm_dict=None,
@@ -85,8 +149,8 @@ def setup_validation_data(norm_dict=None,
                           **kwargs):
     n_workers = 0
 
-    train_dataset = Dataset(_C.PATHS.TRAIN_DATA_LABEL, joint, norm_dict, **kwargs)
-    test_dataset = Dataset(_C.PATHS.TEST_DATA_LABEL, joint, norm_dict, **kwargs)
+    train_dataset = Dataset(_C.PATHS.TRAIN_DATA_LABEL, joint, norm_dict, is_train=True, **kwargs)
+    test_dataset = Dataset(_C.PATHS.TEST_DATA_LABEL, joint, norm_dict, is_train=False, **kwargs)
     train_dloader = torch.utils.data.DataLoader(
         train_dataset,
         batch_size=batch_size // 2,
@@ -94,11 +158,95 @@ def setup_validation_data(norm_dict=None,
         shuffle=True,
         pin_memory=True,
     )
+    
     test_dloader = torch.utils.data.DataLoader(
         test_dataset,
-        batch_size=batch_size // 2,
+        batch_size=1,
         num_workers=n_workers,
-        shuffle=True,
+        shuffle=False,
         pin_memory=True,
     )
+    
     return train_dloader, test_dloader
+    
+    
+    
+def setup_confirmation_data(norm_dict=None,
+                          joint=None,
+                          batch_size=None,
+                          **kwargs):
+    n_workers = 0
+
+    all_dataset = Dataset(_C.PATHS.TEST_DATA_LABEL, joint, norm_dict, is_train=False, **kwargs)
+
+    # acl_dataset = Dataset(_C.PATHS.ACL_DATA_LABEL, joint, norm_dict, is_train=False, **kwargs)
+    # healthy_dataset = Dataset(_C.PATHS.HEALTHY_DATA_LABEL, joint, norm_dict, is_train=False, **kwargs)
+    all_dloader = torch.utils.data.DataLoader(
+        all_dataset,
+        batch_size=1,
+        num_workers=n_workers,
+        shuffle=False,
+        pin_memory=True,
+    )
+    
+    # acl_dloader = torch.utils.data.DataLoader(
+        # acl_dataset,
+        # batch_size=1,
+        # num_workers=n_workers,
+        # shuffle=False,
+        # pin_memory=True,
+    # )
+    
+    # healthy_dloader = torch.utils.data.DataLoader(
+        # healthy_dataset,
+        # batch_size=1,
+        # num_workers=n_workers,
+        # shuffle=False,
+        # pin_memory=True,
+    # )
+    
+    return all_dloader#, acl_dloader,healthy_dloader
+    
+
+def setup_overground_data(norm_dict=None,joint=None, batch_size=None,**kwargs):
+    n_workers = 0
+
+    all_dataset = Dataset(_C.PATHS.EXTRA_SUBJECTS, joint, norm_dict, is_train=False,is_remote=True, **kwargs)
+
+    all_dloader = torch.utils.data.DataLoader(
+        all_dataset,
+        batch_size=1,
+        num_workers=n_workers,
+        shuffle=False,
+        pin_memory=True,
+    )
+    
+
+    return all_dloader#, acl_dloader,healthy_dloader   
+    
+    
+def setup_remote_data(norm_dict=None,
+                          joint=None,
+                          batch_size=None,
+                          **kwargs):
+    n_workers = 0
+
+    all_dataset = Dataset(_C.PATHS.ALL_DATA_LABEL, joint, norm_dict, is_train=False,is_remote=True, **kwargs)
+    print(len(all_dataset))
+
+    all_dloader = torch.utils.data.DataLoader(
+        all_dataset,
+        batch_size=1,
+        num_workers=n_workers,
+        shuffle=False,
+        pin_memory=True,
+    )
+    
+    
+    
+    return all_dloader
+    
+    
+    
+  
+
--- lib/data/preprocess_data.py
+++ lib/data/preprocess_data.py
@@ -11,61 +11,322 @@ import os.path as osp
 
 import torch
 import numpy as np
+from scipy.signal import find_peaks
+import matplotlib.pyplot as plt
 
 from pdb import set_trace as st
 
 
-def load_imu(data_dir):
-    pass
 
-
-def preprocess(subjects, is_train):
-    out_fname = _C.PATHS.TRAIN_DATA_LABEL if is_train else _C.PATHS.TEST_DATA_LABEL
-    imus, angles, seqs, seq_idx = [], [], [], 0
+def preprocess(subjects, is_train,group_label):
+    buffer = 500
+    if group_label == False:
+        out_fname = _C.PATHS.TRAIN_DATA_LABEL if is_train else _C.PATHS.TEST_DATA_LABEL
+    if group_label == 'ACL':
+        out_fname = _C.PATHS.ACL_DATA_LABEL
+        
+    if group_label == 'HEALTHY':
+        out_fname = _C.PATHS.HEALTHY_DATA_LABEL
+    if group_label == 'ALL':
+        out_fname = _C.PATHS.ALL_DATA_LABEL
+    imus, angles, subj_act, seqs, seq_idx = [], [], [],[], 0
 
     for subject in subjects:
         subject_dir = glob.glob(osp.join(_C.PATHS.DATA, f'{subject}*'))[0]
-        _, trials, _ = next(os.walk(subject_dir))
+        #_, trials, _ = next(os.walk(subject_dir))
+        trials = [activity for activity in os.listdir(subject_dir) if 'Day' not in activity]
         for trial in trials:
             trial_dir = osp.join(subject_dir, trial)
 
             # Load IMU data
             temp_imus = []
             imu_dir = osp.join(subject_dir, trial, 'IMU')
-            for sensor in _C.DATA.IMU_LIST:
-                acc = np.load(osp.join(imu_dir, sensor, f'{sensor} acc.npy'))
-                gyr = np.load(osp.join(imu_dir, sensor, f'{sensor} gyr.npy'))
-                imu = torch.from_numpy(np.concatenate((acc, gyr), axis=-1)).float()
-                temp_imus.append(imu.unsqueeze(1))
+            # fig,axs = plt.subplots(4)
+            
+            if subject == 'PS005':
+                IMU_LIST = ['left posterior shank',
+                'left posterior thigh',
+                'right posterior shank',
+                'right posterior thigh',]
+                for val,sensor in enumerate(IMU_LIST):
+                    acc = np.load(osp.join(imu_dir, sensor, f'{sensor} acc_trimmed.npy'))
+                    gyr = np.load(osp.join(imu_dir, sensor, f'{sensor} gyr_trimmed.npy'))
+                    imu = torch.from_numpy(np.concatenate((acc, gyr), axis=-1)).float()
+                    temp_imus.append(imu[buffer:].unsqueeze(1))
+            else:
+                for val,sensor in enumerate(_C.DATA.IMU_LIST):
+                    acc = np.load(osp.join(imu_dir, sensor, f'{sensor} acc_trimmed.npy'))
+                    gyr = np.load(osp.join(imu_dir, sensor, f'{sensor} gyr_trimmed.npy'))
+                    imu = torch.from_numpy(np.concatenate((acc, gyr), axis=-1)).float()
+                    temp_imus.append(imu[buffer:].unsqueeze(1))
+
+                # axs[val].plot(gyr[0:600,:],label=sensor+' gyr')
+                # axs[val].set_ylabel(sensor+' gyr')
+            # fig.savefig('imus.png')
             temp_imus = torch.cat(temp_imus, dim=1)
             imus.append(temp_imus)
 
             # Load Mocap angle data
             temp_angles = []
             mocap_dir = osp.join(subject_dir, trial, 'Mocap')
-            for joint in _C.DATA.JOINT_LIST:
-
+            # fig,axs = plt.subplots(2)
+            for val,joint in enumerate(_C.DATA.JOINT_LIST):
                 #MOCAP CS X-adduction/abduction Y- internal/external rotation Z-flexion/extension
-                angle = np.load(osp.join(mocap_dir, joint, f'{joint} angle.npy'))
+                angle = np.load(osp.join(mocap_dir, joint, f'{joint} angle_trimmed.npy'))
                 #change to IK code output set up outputted from from IK code
                 angle[:,[0,1,2]] = angle[:,[2,0,1]]
-                temp_angles.append(torch.from_numpy(angle).unsqueeze(1).float())
+                temp_angles.append(torch.from_numpy(angle[buffer:]).unsqueeze(1).float())
+                # axs[val].plot(angle[0:600,:],label=joint)
+                # axs[val].set_ylabel(joint)
+            # fig.savefig('mocap.png')
             temp_angles = torch.cat(temp_angles, dim=1)
             angles.append(temp_angles)
+            subj_act.append(subject+'_'+trial)
 
             # Put label to each trial
             seqs.append((torch.ones(temp_angles.shape[0]) * seq_idx).to(dtype=torch.int16))
             seq_idx += 1
-
+    #import pdb; pdb.set_trace()        
+    imu = torch.cat(imus, dim=0)
+    angle = torch.cat(angles, dim=0)
     torch.save({
-        'imu': torch.cat(imus, dim=0),
-        'angle': torch.cat(angles, dim=0),
+        'imu': imu,
+        'angle': angle,
+        'label':subj_act,
+        'seq': torch.cat(seqs, dim=0)},
+        out_fname
+    )
+def preprocess_train(subjects, is_train,group_label):
+    buffer = 500
+    if group_label == False:
+        out_fname = _C.PATHS.TRAIN_DATA_LABEL if is_train else _C.PATHS.TEST_DATA_LABEL
+    if group_label == 'ACL':
+        out_fname = _C.PATHS.ACL_DATA_LABEL
+        
+    if group_label == 'HEALTHY':
+        out_fname = _C.PATHS.HEALTHY_DATA_LABEL
+    if group_label == 'ALL':
+        out_fname = _C.PATHS.ALL_DATA_LABEL
+    imus, angles, subj_act, seqs, seq_idx = [], [], [],[], 0
+
+    for subject in subjects:
+        subject_dir = glob.glob(osp.join(_C.PATHS.DATA, f'{subject}*'))[0]
+        #_, trials, _ = next(os.walk(subject_dir))
+        trials = [activity for activity in os.listdir(subject_dir) if 'Day' not in activity]
+        for trial in trials:
+            trial_dir = osp.join(subject_dir, trial)
+
+            # Load IMU data
+            temp_imus = []
+            imu_dir = osp.join(subject_dir, trial, 'IMU')
+            # fig,axs = plt.subplots(4)
+            plt.figure()
+            if subject == 'PS005':
+                print('yeah')
+                IMU_LIST = ['left posterior shank',
+                'left posterior thigh',
+                'right posterior shank',
+                'right posterior thigh',]
+                for val,sensor in enumerate(IMU_LIST):
+                    acc = np.load(osp.join(imu_dir, sensor, f'{sensor} acc_trimmed.npy'))
+                    gyr = np.load(osp.join(imu_dir, sensor, f'{sensor} gyr_trimmed.npy'))
+                    if is_train:
+                        start = 0
+                        end = int(len(acc)*0.8)
+                    else:
+                        start= int(len(acc)*0.8)
+                        end =-1
+                    if 'left posterior shank' in sensor:
+                        plt.plot(gyr[0:800,2],label='IMU')
+                    imu = torch.from_numpy(np.concatenate((acc, gyr), axis=-1)).float()
+                    temp_imus.append(imu[start:end].unsqueeze(1))
+            else:
+                for val,sensor in enumerate(_C.DATA.IMU_LIST):
+                    acc = np.load(osp.join(imu_dir, sensor, f'{sensor} acc_trimmed.npy'))
+                    gyr = np.load(osp.join(imu_dir, sensor, f'{sensor} gyr_trimmed.npy'))
+                    if is_train:
+                        start = 0
+                        end = int(len(acc)*0.8)
+                    else:
+                        start= int(len(acc)*0.8)
+                        end =-1
+                    imu = torch.from_numpy(np.concatenate((acc, gyr), axis=-1)).float()
+
+                    temp_imus.append(imu[start:end].unsqueeze(1))
+
+                    if 'left lateral shank' in sensor:
+                        plt.plot(gyr[0:800,2],label='IMU')
+                # axs[val].plot(gyr[0:600,:],label=sensor+' gyr')
+                # axs[val].set_ylabel(sensor+' gyr')
+            # fig.savefig('imus.png')
+            
+            temp_imus = torch.cat(temp_imus, dim=1)
+            imus.append(temp_imus)
+
+            # Load Mocap angle data
+            temp_angles = []
+            mocap_dir = osp.join(subject_dir, trial, 'Mocap')
+            # fig,axs = plt.subplots(2)
+            for val,joint in enumerate(_C.DATA.JOINT_LIST):
+                #MOCAP CS X-adduction/abduction Y- internal/external rotation Z-flexion/extension
+                angle = np.load(osp.join(mocap_dir, joint, f'{joint} angle_trimmed.npy'))
+                #change to IK code output set up outputted from from IK code
+                angle[:,[0,1,2]] = angle[:,[2,0,1]]
+                if is_train:
+                        start = 0
+                        end = int(len(angle)*0.8)
+                        print(end)
+                        #print('length 80%:'+str(end))
+                else:
+                        start= int(len(angle)*0.8)
+                        end =-1
+                        #print('length 20%:'+str(len(angle)-int(len(angle)*0.8)))
+                #print(len(angle))
+                temp_angles.append(torch.from_numpy(angle[start:end]).unsqueeze(1).float())
+                
+                if 'l' in joint:
+                    plt.plot(angle[0:800,0],label='ground truth')
+                plt.legend()
+                plt.savefig(subject+trial+'.png')
+                # axs[val].set_ylabel(joint)
+            # fig.savefig('mocap.png')
+            temp_angles = torch.cat(temp_angles, dim=1)
+            angles.append(temp_angles)
+            subj_act.append(subject+'_'+trial)
+
+            # Put label to each trial
+            seqs.append((torch.ones(temp_angles.shape[0]) * seq_idx).to(dtype=torch.int16))
+            seq_idx += 1
+            #import pdb; pdb.set_trace()
+    #import pdb; pdb.set_trace()        
+    imu = torch.cat(imus, dim=0)
+    angle = torch.cat(angles, dim=0)
+    torch.save({
+        'imu': imu,
+        'angle': angle,
+        'label':subj_act,
+        'seq': torch.cat(seqs, dim=0)},
+        out_fname
+    )  
+    
+def preprocess_two_overground(subjects, is_train,group_label):
+
+    if group_label == False:
+        out_fname = _C.PATHS.TRAIN_DATA_LABEL if is_train else _C.PATHS.TEST_DATA_LABEL
+    if group_label == 'ACL':
+        out_fname = _C.PATHS.ACL_DATA_LABEL
+    if group_label == 'EXTRA_SUBJECTS':
+        out_fname = _C.PATHS.EXTRA_SUBJECTS
+    if group_label == 'HEALTHY':
+        out_fname = _C.PATHS.HEALTHY_DATA_LABEL
+    if group_label == 'ALL':
+        out_fname = _C.PATHS.ALL_DATA_LABEL
+    imus, subj_act, seqs, seq_idx = [], [],[], 0
+
+    for subject in subjects:
+        subject_dir = glob.glob(osp.join(_C.PATHS.DATA, f'{subject}*'))[0]
+        trials = [day for day in os.listdir(subject_dir) if 'Overground' in day]
+        for trial in trials:
+            trial_dir = osp.join(subject_dir, trial)
+            for bout in os.listdir(trial_dir):
+
+                # Load IMU data
+                temp_imus = []
+                imu_dir = osp.join(subject_dir, trial, bout)
+                # fig,axs = plt.subplots(4)
+
+                for val,sensor in enumerate(_C.DATA.IMU_LIST):
+                    acc = np.load(osp.join(imu_dir, sensor, f'{sensor} acc_trimmed.npy'))
+                    gyr = np.load(osp.join(imu_dir, sensor, f'{sensor} gyr_trimmed.npy'))
+                    start= int(len(acc)*0.8)
+                    end =-1
+                    imu = torch.from_numpy(np.concatenate((acc, gyr), axis=-1)).float()
+                    temp_imus.append(imu[start:end].unsqueeze(1))
+
+                    # axs[val].plot(gyr[0:600,:],label=sensor+' gyr')
+                    # axs[val].set_ylabel(sensor+' gyr')
+                # fig.savefig('imus.png')
+                temp_imus = torch.cat(temp_imus, dim=1)
+                imus.append(temp_imus)
+
+                subj_act.append(subject+'_'+trial+'_'+bout)
+
+                # Put label to each trial
+                seqs.append((torch.ones(temp_imus.shape[0]) * seq_idx).to(dtype=torch.int16))
+                seq_idx += 1
+    #import pdb; pdb.set_trace()  
+
+    imu = torch.cat(imus, dim=0)
+    torch.save({
+        'imu': imu,
+        'label':subj_act,
+        'seq': torch.cat(seqs, dim=0)},
+        out_fname
+    )
+
+def preprocess_remote(subjects, is_train,group_label):
+
+    if group_label == False:
+        out_fname = _C.PATHS.TRAIN_DATA_LABEL if is_train else _C.PATHS.TEST_DATA_LABEL
+    if group_label == 'ACL':
+        out_fname = _C.PATHS.ACL_DATA_LABEL
+        
+    if group_label == 'HEALTHY':
+        out_fname = _C.PATHS.HEALTHY_DATA_LABEL
+    if group_label == 'ALL':
+        out_fname = _C.PATHS.ALL_DATA_LABEL
+    imus, subj_act, seqs, seq_idx = [], [],[], 0
+
+    for subject in subjects:
+        subject_dir = glob.glob(osp.join(_C.PATHS.DATA, f'{subject}*'))[0]
+        trials = [day for day in os.listdir(subject_dir) if 'Day' in day]
+        for trial in trials:
+            trial_dir = osp.join(subject_dir, trial)
+            for bout in os.listdir(trial_dir):
+
+                # Load IMU data
+                temp_imus = []
+                imu_dir = osp.join(subject_dir, trial, bout)
+                # fig,axs = plt.subplots(4)
+
+                for val,sensor in enumerate(_C.DATA.IMU_LIST):
+                    acc = np.load(osp.join(imu_dir, sensor, f'{sensor} acc_trimmed.npy'))
+                    print(acc)
+                    gyr = np.load(osp.join(imu_dir, sensor, f'{sensor} gyr_trimmed.npy'))
+                    imu = torch.from_numpy(np.concatenate((acc, gyr), axis=-1)).float()
+                    temp_imus.append(imu.unsqueeze(1))
+
+                    # axs[val].plot(gyr[0:600,:],label=sensor+' gyr')
+                    # axs[val].set_ylabel(sensor+' gyr')
+                # fig.savefig('imus.png')
+                temp_imus = torch.cat(temp_imus, dim=1)
+                imus.append(temp_imus)
+
+                subj_act.append(subject+'_'+trial+'_'+bout)
+
+                # Put label to each trial
+                seqs.append((torch.ones(temp_imus.shape[0]) * seq_idx).to(dtype=torch.int16))
+                seq_idx += 1
+    #import pdb; pdb.set_trace()  
+    print(len(imu))
+    imu = torch.cat(imus, dim=0)
+    torch.save({
+        'imu': imu,
+        'label':subj_act,
         'seq': torch.cat(seqs, dim=0)},
         out_fname
     )
 
 if __name__ == '__main__':
-    train_subjects = ['HS001', 'HS003', 'HS004', 'HS005','PS001','PS002','PS003','PS004',]
-    test_subjects = ['HS006', 'HS007','PS006']
-    preprocess(train_subjects, True)
-    preprocess(test_subjects, False)
+    train_subjects = ['PS001','PS005', 'PS004', 'PS002','HS001','HS005','HS006','HS007']
+    test_subjects = ['PS006','HS003','HS004','PS003',]
+    #preprocess(train_subjects, True,False)
+    #preprocess(test_subjects, False,False)
+    # preprocess_train(['PS001', 'PS003', 'PS004', 'PS002','PS005','HS001','HS005','HS006','HS007','PS006','HS003','HS004'], True,False)
+    # preprocess_train(['PS001', 'PS003', 'PS004', 'PS002','PS005','HS001','HS005','HS006','HS007','PS006','HS003','HS004'], False,False)
+    
+    preprocess_two_overground(['PS001','HS003'], False,'EXTRA_SUBJECTS')
+    # preprocess(['PS001', 'PS003', 'PS004', 'PS002','PS005','PS006'], False,'ACL')
+    # preprocess(['HS001','HS005','HS006','HS007','HS003','HS004'], False,'HEALTHY')
+    #preprocess_remote(['PS001', 'PS003', 'PS004', 'PS002','PS005','HS001','HS005','HS006','HS007','PS006','HS003','HS004'], False,'ALL')
+    #preprocess_remote(['HS006'], False,'ALL')
