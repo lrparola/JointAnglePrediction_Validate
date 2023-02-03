128f9059dddb2a3fd174147aaa93431b77cfd9ac
Lauren Parola <88797808+lrparola@users.noreply.github.com>
2023-02-03 10:11:01 -0500

    Add files via upload

--- configs/constants.py
+++ configs/constants.py
@@ -5,6 +5,10 @@ class PATHS:
     DATA = 'dataset/IMU_Mocap_Output_Data'
     TRAIN_DATA_LABEL = 'dataset/IMU_Mocap_Output_Data/train_data_label.pt'
     TEST_DATA_LABEL = 'dataset/IMU_Mocap_Output_Data/test_data_label.pt'
+    ACL_DATA_LABEL = 'dataset/IMU_Mocap_Output_Data/acl_data_label.pt'
+    HEALTHY_DATA_LABEL = 'dataset/IMU_Mocap_Output_Data/healthy_data_label.pt'
+    ALL_DATA_LABEL = 'dataset/IMU_Mocap_Output_Data/all_data_label.pt'
+    EXTRA_SUBJECTS = 'dataset/IMU_Mocap_Output_Data/extra_subjects.pt'
 
 
 class DATA:
